6827d3dd54aa36484fdcada940eb55f40685981d
vijanatech <vijanatech@gmail.coml>
2023-03-20 14:56:58 +0300

    new

--- forms/contact.php
+++ forms/contact.php
@@ -1,8 +1,8 @@
 <?php
  
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // Your real receiving email address
+  $receiving_email_address = 'contact@personalfinancehub.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
